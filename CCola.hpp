b7c505a9a0a512fd4e8802d192a77b916d842a5e
gonzaloLaqui147 <55416564+gonzaloLaqui147@users.noreply.github.com>
2020-05-05 01:05:51 -0500

    Update CCola.hpp

--- CCola.hpp
+++ CCola.hpp
@@ -54,57 +54,62 @@ public:
 	}
 
 	void priorizar() {
-		if (tam == 0) { cout << "No existen personas registrados\n"; cin.get(); }
-		if (tam == 1) { cout << "Solo existe una persona en la cola\n"; cin.get(); }
+		if (tam == 0) { cout << "No existen personas registradas en el sistema\n"; cin.get(); }
 		else {
-			Nodo<T>* aux = inicio;
-			Nodo<T>* aux2 = aux;
-			string nombre, destino; int edad, DNI, prioridad;
-			bool parada = true;
+			if (tam == 1) { cout << "Solo existe una persona en la cola\n"; cin.get(); }
+			else {
+				Nodo<T>* aux = inicio;
+				Nodo<T>* aux2 = aux;
+				string nombre, destino; int edad, DNI, prioridad;
+				bool parada = true;
 
-			nombre = aux->e->getNombre(); destino = aux->e->getDestino(); edad = aux->e->getEdad(); DNI = aux->e->getDNI(); prioridad = aux->e->getPrioridad();
+				nombre = aux->e->getNombre(); destino = aux->e->getDestino(); edad = aux->e->getEdad(); DNI = aux->e->getDNI(); prioridad = aux->e->getPrioridad();
 
-			
-			if (inicio->e->getPrioridad() == 0) {
-				for (; !is_Empty() && parada == true;) {
-					if (aux2->e->getPrioridad() == 1) {
-						inicio->e->setNombre(aux2->e->getNombre());
-						inicio->e->setDestino(aux2->e->getDestino());
-						inicio->e->setEdad(aux2->e->getEdad());
-						inicio->e->setDNI(aux2->e->getDNI());
-						inicio->e->setPrioridad(aux2->e->getPrioridad());
 
-						aux2->e->setNombre(nombre);
-						aux2->e->setDestino(destino);
-						aux2->e->setEdad(edad);
-						aux2->e->setDNI(DNI);
-						aux2->e->setPrioridad(prioridad);
-						parada = false;
+				if (inicio->e->getPrioridad() == 0) {
+					for (; !is_Empty() && parada == true;) {
+						if (aux2->e->getPrioridad() == 1) {
+							inicio->e->setNombre(aux2->e->getNombre());
+							inicio->e->setDestino(aux2->e->getDestino());
+							inicio->e->setEdad(aux2->e->getEdad());
+							inicio->e->setDNI(aux2->e->getDNI());
+							inicio->e->setPrioridad(aux2->e->getPrioridad());
+
+							aux2->e->setNombre(nombre);
+							aux2->e->setDestino(destino);
+							aux2->e->setEdad(edad);
+							aux2->e->setDNI(DNI);
+							aux2->e->setPrioridad(prioridad);
+							parada = false;
+						}
+						else {
+							aux2 = aux2->s;
+						}
+					}
+					aux2->a->s = aux2->s;
+					aux2->s = inicio->s;
+					inicio->s->a = aux2;
+					inicio->s = aux2;
+					aux2->a = inicio;
+				}
+
+
+				for (int i = 0; i < tam - 1; i++) {
+					if (aux->s->e->getPrioridad() == 0) {
+						aux2 = aux->s;
+						aux->s = aux2->s;
+						fin->s = aux2;
+						fin = aux2;
 					}
 					else {
-						aux2 = aux2->s;
+						aux = aux->s;
 					}
 				}
-			}
-			aux2->a->s = aux2->s;
-			aux2->s = inicio->s;
-			inicio->s->a = aux2;
-			inicio->s = aux2;
-			aux2->a = inicio;
 
-			for (int i = 0; i < tam - 1; i++) {
-				if(aux->s->e->getPrioridad() == 0){
-					aux2 = aux->s;
-					aux->s = aux2->s;
-					fin->s = aux2;
-					fin = aux2;
-				}
-				else {
-					aux = aux->s;
-				}
 			}
-
+			mostrar();
 		}
+		
 	}
 
 	T getNodo() {
@@ -128,6 +133,16 @@ public:
 			}
 		}
 		cout << endl;
+	}
+
+	void vaciar_Cola() {
+
+		for (; !is_Empty();) {
+			Nodo<T>* aux = inicio;
+			inicio = inicio->s;
+			delete aux;
+			tam--;
+		}
 	}
 
 	int getTam() { return tam; }
